fb59604f0d2309eb1202293922457a9e08626bf6
Ethan Fann <ecfann@gmail.com>
2020-02-08 11:20:03 -0800

    Remove download

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,14 +4,12 @@ import { StaticQuery, graphql } from "gatsby";
 import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import styledNormalize from "styled-normalize";
-import { Download as DLIcon } from "styled-icons/fa-solid/Download.cjs";
 
 import "typeface-pacifico";
 import "typeface-raleway";
 import "typeface-open-sans";
 
 import Navbar from "./Navbar";
-import media from "../utils/style";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -46,25 +44,6 @@ const GlobalStyle = createGlobalStyle`
 
 `;
 
-const DL = styled.a`
-  border: none;
-  color: white;
-  padding: 12px 30px;
-  cursor: pointer;
-  font-size: 20px;
-  width: 60%;
-  margin: 0 auto;
-  ${media.xs`
-      width:100%;
-    `}
-  svg {
-    color: white;
-    width: 1.5em;
-    height: 1.5m;
-    padding-right: 10px;
-  }
-`;
-
 const Footer = styled.footer`
   display: flex;
   flex-direction: column;
@@ -72,9 +51,6 @@ const Footer = styled.footer`
   text-align: center;
   color: #fff;
   background-color: #000;
-  img {
-    margin-bottom: 0;
-  }
 `;
 
 const Body = styled.div`
@@ -121,10 +97,7 @@ const Layout = ({ children, noMenu }) => (
           <Navbar noMenu={noMenu} edges={data.allSocialJson.edges} />
           {children}
           <Footer>
-            <DL href="/resume.pdf">
-              <DLIcon />
-              Resume
-            </DL>
+            ethanfann.com
           </Footer>
         </Body>
       </>
