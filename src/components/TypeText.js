5f4b8c66d777d5f635337012a6c87c9e43cb30fe
Ethan Fann <github@ethanfann.com>
2021-10-17 16:43:32 -0700

    Update TypeText

--- src/components/TypeText.js
+++ src/components/TypeText.js
@@ -20,48 +20,12 @@ const StyledTypist = styled(Typist)`
   `}
 `;
 
-const TypeText = (props) => {
-  const { text } = props;
+const TypeText = () => {
   const typistStrings = {
-    First: "Build Responsive Web Apps",
-    Second: "Integrate APIs",
-    Third: "Provide Top Tier Technical Support",
-    Fourth: "Love to Ride Bikes",
-    Fifth: "Want to Provide Delightful Tech Experiences",
+    First: "Welcome",
   };
-  if (text) {
-    return (
-      <StyledTypist cursor={{ show: false }}>
-        <strong>{text.split(" ").slice(0, 1)}</strong>
-        <br />
-        {text
-          .split(" ")
-          .slice(1)
-          .join(" ")}
-      </StyledTypist>
-    );
-  }
   return (
-    <StyledTypist cursor={{ show: false }}>
-      <strong key={1}>I </strong>
-      {typistStrings.First}
-      <Typist.Backspace count={typistStrings.First.length} delay={1000} />
-      <span key={2}>{typistStrings.Second}</span>
-      <Typist.Backspace count={typistStrings.Second.length} delay={1000} />
-      <span key={3}>{typistStrings.Third}</span>
-      <Typist.Backspace count={typistStrings.Third.length} delay={1000} />
-      <span key={4}>{typistStrings.Fourth}</span>
-      <Typist.Delay ms={300} />
-      <span key={5}>.</span>
-      <Typist.Delay ms={600} />
-      <span key={6}>.</span>
-      <Typist.Delay ms={900} />
-      <span key={7}>.</span>
-      <Typist.Delay ms={300} />
-      <Typist.Backspace count={typistStrings.Fourth.length + 5} delay={10} />
-      <strong key={1}>But I Mostly </strong>
-      {typistStrings.Fifth}
-    </StyledTypist>
+    <StyledTypist cursor={{ show: false }}>{typistStrings.First}</StyledTypist>
   );
 };
 
