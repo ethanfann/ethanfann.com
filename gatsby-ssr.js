734e5fc510e80ca9c22811a42d56a6da3b893bbc
Ethan Fann <6005240+ethanfann@users.noreply.github.com>
2022-10-04 22:28:35 -0700

    Another

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,11 @@
 const React = require('react')
 
-exports.onRenderBody = ({ setPreBodyComponents }) => {
+exports.onRenderBody = ({ setPreBodyComponents, setHeadComponents }) => {
   setPreBodyComponents([
     <noscript key="noscript">Your browser does not support JavaScript!</noscript>,
   ])
 
-  return setHeadComponents([
-    <script
-        src={`https://cmp-uat.osano.com/1t0AvyuRhBFceA1OUG/7e28edbe-b626-414e-ba2e-4d7e1d6939e1/osano.js`}
-    />
+  setHeadComponents([
+    <script src="https://cmp-uat.osano.com/1t0AvyuRhBFceA1OUG/7e28edbe-b626-414e-ba2e-4d7e1d6939e1/osano.js"></script>
 ]);
 }
